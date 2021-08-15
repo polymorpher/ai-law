b0a4cc62f11da4309eb50ac8a58d2809a358d60e
Aaron Li <aaron@qokka.ai>
2021-08-15 04:06:37 -0700

    extractor: single extract func; fix bugs

--- extractor.py
+++ extractor.py
@@ -7,21 +7,19 @@ import holding_extractor
 
 
 class Extractor:
-    def __init__(self, data_path=CACHE, out_path=CACHE_OUT, text_out_path=CACHE_OUT,
-                 holiding_out_path=HOLDING_OUT_PATH):
+    def __init__(self, data_path=CACHE, text_out_path=CACHE_OUT,
+                 holiding_out_path=CACHE_HOLDING):
         self.nlp = nlp_init()
         self.data_path = data_path
-        self.out_path = out_path
         self.text_out_path = text_out_path
         self.holiding_out_path = holiding_out_path
+        self.cleaner = html_extractor.build_cleaner()
+        self.parser = html_extractor.build_parser()
         os.makedirs(data_path, exist_ok=True)
-        os.makedirs(out_path, exist_ok=True)
         os.makedirs(text_out_path, exist_ok=True)
         os.makedirs(holiding_out_path, exist_ok=True)
 
-    def local_extract_text(self):
-        cleaner = html_extractor.build_cleaner()
-        parser = html_extractor.build_parser()
+    def local_extract_text_all(self):
         files = os.listdir(Path(self.data_path))
         files = sorted(files)
         for fname in files:
@@ -29,14 +27,19 @@ class Extractor:
                 continue
             if not fname.endswith('.html'):
                 continue
-            p = os.path.join(self.data_path, fname)
-            print(f'Reading {p}')
-            html_content = Path(p).read_text()
-            text, texts = html_extractor.extract_html(html_content, parser=parser, cleaner=cleaner)
-            o = os.path.join(self.out_path, fname.split('.')[0] + '.txt')
-            Path(o).write_text(text)
+            self.local_extract_text(fname)
 
-    def local_extract_holdings(self, ):
+    def local_extract_text(self, fname):
+        p = os.path.join(self.data_path, fname)
+        print(f'Reading {p}')
+        html_content = Path(p).read_text()
+        text, texts = html_extractor.extract_html(html_content, parser=self.parser, cleaner=self.cleaner)
+        if not text:
+            return
+        o = os.path.join(self.text_out_path, fname.split('.')[0] + '.txt')
+        Path(o).write_text(text)
+
+    def local_extract_holdings_all(self):
         files = os.listdir(Path(self.text_out_path))
         files = sorted(files)
         combined = []
@@ -45,6 +48,7 @@ class Extractor:
                 continue
             if not fname.endswith('.txt'):
                 continue
+            print(f'Processing {fname}')
             p = os.path.join(self.text_out_path, fname)
             text = Path(p).read_text()
             holdings = holding_extractor.extract_holding(text, self.nlp)
