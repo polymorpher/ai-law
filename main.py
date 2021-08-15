8674bd8e331d0ecafe4528f69d04d51c9bcb192a
Aaron Li <aaron@qokka.ai>
2021-08-15 04:30:50 -0700

    main: full pipeline

--- main.py
+++ main.py
@@ -1,90 +1,57 @@
 from vars import *
-from lxml import html
-import os
-from pathlib import Path
-from lxml.html.clean import Cleaner
+from extractor import Extractor
+from feeder import AWSFeeder
 import re
-import spacy
-
-
-def extract_text():
-    cleaner = Cleaner()
-    cleaner.javascript = True
-    cleaner.style = True
-    files = os.listdir(Path(DATA_PATH))
-    files = sorted(files)
-    parser = html.HTMLParser(encoding='ascii')
-    for fname in files:
-        if fname.startswith('.'):
-            continue
-        if not fname.endswith('.html'):
-            continue
-        p = os.path.join(DATA_PATH, fname)
-        print(f'Reading {p}')
-        tree = html.parse(p, parser=parser).getroot()
-        selected = tree.cssselect('.opinion_text')
-        texts = []
-        if len(selected) < 1:
-            print(f'Skipped {p}')
-            continue
-        for el in selected:
-            cleaned_tree = cleaner.clean_html(el)
-            text = cleaned_tree.text_content().__str__()
-            text = re.sub(r'[\n]?\[\*[0-9]+\][\n]?', '', text)
-            # text = re.sub(r'\n+([a-zA-Z0-9,. ]+)', ' \g<1>', text)
-            text = re.sub(r'[^\x09-\x7f]', '', text)
-            text = re.sub(r'\n', ' ', text)
-            text = re.sub(r'[ \t]+', ' ', text)
-            texts.append(text)
-
-        text = '\n'.join(texts)
-        # print(text)
-        # input('Press any key to continue:')
-        o = os.path.join(TEXT_OUT_PATH, fname.split('.')[0] + '.txt')
-        Path(o).write_text(text)
-
-
-def extract_holdings():
-    files = os.listdir(Path(TEXT_OUT_PATH))
-    files = sorted(files)
-    nlp = spacy.load('en_core_web_sm', disable=['ner', 'textcat'])
-    print(nlp.pipe_names)
-    combined = []
-    for fname in files:
-        if fname.startswith('.'):
-            continue
-        if not fname.endswith('.txt'):
-            continue
-        p = os.path.join(TEXT_OUT_PATH, fname)
-        text = Path(p).read_text()
-        doc = nlp(text)
-
-        sents = list(doc.sents)
-        holdings = []
-        for i in range(len(sents)):
-            sent = sents[i]
-            start, end = sent.start, sent.end
-            tokens = doc[start:end]
-            for t in tokens:
-                if t.lemma_ == 'hold' and ((not USE_POS) or t.pos_ == 'VERB'):
-                    ii = max(i - 1, 0)
-                    jj = min(i + 1, len(sents) - 1)
-                    start, end = sents[ii].start, sents[jj].end
-                    text = doc[start:end].text
-                    text = re.sub(r'\n', ' ', text)
-                    holdings.append(text)
-        if len(holdings) > 0:
-            holding_all = ' '.join(holdings)
-            o = os.path.join(HOLDING_OUT_PATH, fname)
-            Path(o).write_text(holding_all)
-            combined.extend(holdings)
-    all = '\n'.join(combined)  # [f'{r}' for r in combined]
-    Path(os.path.join(HOLDING_OUT_PATH, 'all.csv')).write_text(all)
-    # input('Press any key to continue:')
-
 
 if __name__ == '__main__':
-    os.makedirs(TEXT_OUT_PATH, exist_ok=True)
-    os.makedirs(HOLDING_OUT_PATH, exist_ok=True)
-    # extract_text()
-    extract_holdings()
+    feeder = AWSFeeder()
+    keys, dirs = feeder.list(prefix=S3_PATH)
+    case_folder_re = r'.*/([0-9]+)_complete'
+    print('keys', keys)
+    print('dirs', dirs)
+    case_dirs = [d for d in dirs if re.match(case_folder_re, d)]
+    case_years = []
+    for dir in case_dirs:
+        m = re.match(case_folder_re, dir)
+        case_years.append(m.group(1))
+    year_keys, _ = feeder.list(prefix=case_dirs[0])
+
+    print(case_dirs)
+    print(year_keys)
+    extractor = Extractor()
+
+
+    def on_next(filename):
+        frag = filename.split('/')[-1]
+        extractor.local_extract_text(frag)
+
+
+    def on_error(ex: Exception):
+        print(ex)
+
+
+    def key_processor(key):
+        if not key.endswith('.html'):
+            return None
+        return key.split('/')[-1]
+
+
+    # feeder.scan_prefix(prefix=case_dirs[0], key_processor=key_processor, on_next=on_next,
+    #                    on_error=on_error)
+
+    # extractor.local_extract_holdings_all()
+
+    def upload_all(year):
+        files = extractor.list_all_text_files()
+        for f in files:
+            k = S3_TEXT_OUT_PATH + concat(year, f)
+            feeder.upload(key=k, filename=concat(extractor.text_out_path, f))
+        print(f'[{year}] Uploaded {len(files)} extracted text files')
+        files = extractor.list_all_holding_files()
+        for f in files:
+            k = S3_HOLDING_OUT_PATH + concat(year, f)
+            feeder.upload(key=k, filename=concat(extractor.holiding_out_path, f))
+        print(f'[{year}] Uploaded {len(files)} extracted holdings')
+
+
+    upload_all(case_years[0])
